eccc2c30835a8115c676a61924ea944efe07837d
offcn-java <2366890881@qq.com>
2020-12-17 17:24:18 +0800

    Initial commit

---
+++ src/main/java/com/offcn/springsession/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.offcn.springsession.controller;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+@RestController
+public class TestController {
+    @Override
+    public String toString() {
+        System.out.println();
+        return super.toString();
+    }
+}
